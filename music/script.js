3d011fb292c0a0a3e097661b426e52f49e0e022b
nevo <68334687+tcelectronic@users.noreply.github.com>
2020-09-30 21:54:37 +0530

    Add files via upload

--- music/script.js
+++ music/script.js
@@ -1,232 +1,156 @@
-$(function()
-{
-    var playerTrack = $("#player-track"), bgArtwork = $('#bg-artwork'), bgArtworkUrl, albumName = $('#album-name'), trackName = $('#track-name'), albumArt = $('#album-art'), sArea = $('#s-area'), seekBar = $('#seek-bar'), trackTime = $('#track-time'), insTime = $('#ins-time'), sHover = $('#s-hover'), playPauseButton = $("#play-pause-button"),  i = playPauseButton.find('i'), tProgress = $('#current-time'), tTime = $('#track-length'), seekT, seekLoc, seekBarPos, cM, ctMinutes, ctSeconds, curMinutes, curSeconds, durMinutes, durSeconds, playProgress, bTime, nTime = 0, buffInterval = null, tFlag = false, albums = ['Dawn','Me & You','Electro Boy','Home','Proxy (Original Mix)','digu'], trackNames = ['Skylike - Dawn','Alex Skrindo - Me & You','Kaaze - Electro Boy','Jordan Schor - Home','Martin Garrix - Proxy','digu'], albumArtworks = ['_1','_2','_3','_4','_5','_6'], trackUrl = ['https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/2.mp3','https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/1.mp3','https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/3.mp3','https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3','https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/5.mp3','digu.mp3'], playPreviousTrackButton = $('#play-previous'), playNextTrackButton = $('#play-next'), currIndex = -1;
+/*
+design by Voicu Apostol.
+design: https://dribbble.com/shots/3533847-Mini-Music-Player
+I can't find any open music api or mp3 api so i have to download all musics as mp3 file.
+You can fork on github: https://github.com/muhammederdem/mini-player
+*/
 
-    function playPause()
-    {
-        setTimeout(function()
-        {
-            if(audio.paused)
-            {
-                playerTrack.addClass('active');
-                albumArt.addClass('active');
-                checkBuffering();
-                i.attr('class','fas fa-pause');
-                audio.play();
-            }
-            else
-            {
-                playerTrack.removeClass('active');
-                albumArt.removeClass('active');
-                clearInterval(buffInterval);
-                albumArt.removeClass('buffering');
-                i.attr('class','fas fa-play');
-                audio.pause();
-            }
-        },300);
-    }
-
-    	
-	function showHover(event)
-	{
-		seekBarPos = sArea.offset(); 
-		seekT = event.clientX - seekBarPos.left;
-		seekLoc = audio.duration * (seekT / sArea.outerWidth());
-		
-		sHover.width(seekT);
-		
-		cM = seekLoc / 60;
-		
-		ctMinutes = Math.floor(cM);
-		ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
-		
-		if( (ctMinutes < 0) || (ctSeconds < 0) )
-			return;
-		
-        if( (ctMinutes < 0) || (ctSeconds < 0) )
-			return;
-		
-		if(ctMinutes < 10)
-			ctMinutes = '0'+ctMinutes;
-		if(ctSeconds < 10)
-			ctSeconds = '0'+ctSeconds;
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      audio: null,
+      circleLeft: null,
+      barWidth: null,
+      duration: null,
+      currentTime: null,
+      isTimerPlaying: false,
+      tracks: [
         
-        if( isNaN(ctMinutes) || isNaN(ctSeconds) )
-            insTime.text('--:--');
-        else
-		    insTime.text(ctMinutes+':'+ctSeconds);
-            
-		insTime.css({'left':seekT,'margin-left':'-21px'}).fadeIn(0);
-		
-	}
-
-    function hideHover()
-	{
-        sHover.width(0);
-        insTime.text('00:00').css({'left':'0px','margin-left':'0px'}).fadeOut(0);		
-    }
-    
-    function playFromClickedPos()
-    {
-        audio.currentTime = seekLoc;
-		seekBar.width(seekT);
-		hideHover();
-    }
-
-    function updateCurrTime()
-	{
-        nTime = new Date();
-        nTime = nTime.getTime();
-
-        if( !tFlag )
         {
-            tFlag = true;
-            trackTime.addClass('active');
+          name: "Butterflies",
+          artist: "Sia",
+          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/4.jpg",
+          source: "http://pasindu.tk/music/digu.mp3",
+          url: "http://pasindu.tk/music/digu.mp3",
+          favorited: false
+        },
+
+      ],
+      currentTrack: null,
+      currentTrackIndex: 0,
+      transitionName: null
+    };
+  },
+  methods: {
+    play() {
+      if (this.audio.paused) {
+        this.audio.play();
+        this.isTimerPlaying = true;
+      } else {
+        this.audio.pause();
+        this.isTimerPlaying = false;
+      }
+    },
+    generateTime() {
+      let width = (100 / this.audio.duration) * this.audio.currentTime;
+      this.barWidth = width + "%";
+      this.circleLeft = width + "%";
+      let durmin = Math.floor(this.audio.duration / 60);
+      let dursec = Math.floor(this.audio.duration - durmin * 60);
+      let curmin = Math.floor(this.audio.currentTime / 60);
+      let cursec = Math.floor(this.audio.currentTime - curmin * 60);
+      if (durmin < 10) {
+        durmin = "0" + durmin;
+      }
+      if (dursec < 10) {
+        dursec = "0" + dursec;
+      }
+      if (curmin < 10) {
+        curmin = "0" + curmin;
+      }
+      if (cursec < 10) {
+        cursec = "0" + cursec;
+      }
+      this.duration = durmin + ":" + dursec;
+      this.currentTime = curmin + ":" + cursec;
+    },
+    updateBar(x) {
+      let progress = this.$refs.progress;
+      let maxduration = this.audio.duration;
+      let position = x - progress.offsetLeft;
+      let percentage = (100 * position) / progress.offsetWidth;
+      if (percentage > 100) {
+        percentage = 100;
+      }
+      if (percentage < 0) {
+        percentage = 0;
+      }
+      this.barWidth = percentage + "%";
+      this.circleLeft = percentage + "%";
+      this.audio.currentTime = (maxduration * percentage) / 100;
+      this.audio.play();
+    },
+    clickProgress(e) {
+      this.isTimerPlaying = true;
+      this.audio.pause();
+      this.updateBar(e.pageX);
+    },
+    prevTrack() {
+      this.transitionName = "scale-in";
+      this.isShowCover = false;
+      if (this.currentTrackIndex > 0) {
+        this.currentTrackIndex--;
+      } else {
+        this.currentTrackIndex = this.tracks.length - 1;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    nextTrack() {
+      this.transitionName = "scale-out";
+      this.isShowCover = false;
+      if (this.currentTrackIndex < this.tracks.length - 1) {
+        this.currentTrackIndex++;
+      } else {
+        this.currentTrackIndex = 0;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    resetPlayer() {
+      this.barWidth = 0;
+      this.circleLeft = 0;
+      this.audio.currentTime = 0;
+      this.audio.src = this.currentTrack.source;
+      setTimeout(() => {
+        if(this.isTimerPlaying) {
+          this.audio.play();
+        } else {
+          this.audio.pause();
         }
-
-		curMinutes = Math.floor(audio.currentTime / 60);
-		curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
-		
-		durMinutes = Math.floor(audio.duration / 60);
-		durSeconds = Math.floor(audio.duration - durMinutes * 60);
-		
-		playProgress = (audio.currentTime / audio.duration) * 100;
-		
-		if(curMinutes < 10)
-			curMinutes = '0'+curMinutes;
-		if(curSeconds < 10)
-			curSeconds = '0'+curSeconds;
-		
-		if(durMinutes < 10)
-			durMinutes = '0'+durMinutes;
-		if(durSeconds < 10)
-			durSeconds = '0'+durSeconds;
-        
-        if( isNaN(curMinutes) || isNaN(curSeconds) )
-            tProgress.text('00:00');
-        else
-		    tProgress.text(curMinutes+':'+curSeconds);
-        
-        if( isNaN(durMinutes) || isNaN(durSeconds) )
-            tTime.text('00:00');
-        else
-		    tTime.text(durMinutes+':'+durSeconds);
-        
-        if( isNaN(curMinutes) || isNaN(curSeconds) || isNaN(durMinutes) || isNaN(durSeconds) )
-            trackTime.removeClass('active');
-        else
-            trackTime.addClass('active');
-
-        
-		seekBar.width(playProgress+'%');
-		
-		if( playProgress == 100 )
-		{
-			i.attr('class','fa fa-play');
-			seekBar.width(0);
-            tProgress.text('00:00');
-            albumArt.removeClass('buffering').removeClass('active');
-            clearInterval(buffInterval);
-		}
+      }, 300);
+    },
+    favorite() {
+      this.tracks[this.currentTrackIndex].favorited = !this.tracks[
+        this.currentTrackIndex
+      ].favorited;
     }
-    
-    function checkBuffering()
-    {
-        clearInterval(buffInterval);
-        buffInterval = setInterval(function()
-        { 
-            if( (nTime == 0) || (bTime - nTime) > 1000  )
-                albumArt.addClass('buffering');
-            else
-                albumArt.removeClass('buffering');
+  },
+  created() {
+    let vm = this;
+    this.currentTrack = this.tracks[0];
+    this.audio = new Audio();
+    this.audio.src = this.currentTrack.source;
+    this.audio.ontimeupdate = function() {
+      vm.generateTime();
+    };
+    this.audio.onloadedmetadata = function() {
+      vm.generateTime();
+    };
+    this.audio.onended = function() {
+      vm.nextTrack();
+      this.isTimerPlaying = true;
+    };
 
-            bTime = new Date();
-            bTime = bTime.getTime();
-
-        },100);
+    // this is optional (for preload covers)
+    for (let index = 0; index < this.tracks.length; index++) {
+      const element = this.tracks[index];
+      let link = document.createElement('link');
+      link.rel = "prefetch";
+      link.href = element.cover;
+      link.as = "image"
+      document.head.appendChild(link)
     }
-
-    function selectTrack(flag)
-    {
-        if( flag == 0 || flag == 1 )
-            ++currIndex;
-        else
-            --currIndex;
-
-        if( (currIndex > -1) && (currIndex < albumArtworks.length) )
-        {
-            if( flag == 0 )
-                i.attr('class','fa fa-play');
-            else
-            {
-                albumArt.removeClass('buffering');
-                i.attr('class','fa fa-pause');
-            }
-
-            seekBar.width(0);
-            trackTime.removeClass('active');
-            tProgress.text('00:00');
-            tTime.text('00:00');
-
-            currAlbum = albums[currIndex];
-            currTrackName = trackNames[currIndex];
-            currArtwork = albumArtworks[currIndex];
-
-            audio.src = trackUrl[currIndex];
-            
-            nTime = 0;
-            bTime = new Date();
-            bTime = bTime.getTime();
-
-            if(flag != 0)
-            {
-                audio.play();
-                playerTrack.addClass('active');
-                albumArt.addClass('active');
-            
-                clearInterval(buffInterval);
-                checkBuffering();
-            }
-
-            albumName.text(currAlbum);
-            trackName.text(currTrackName);
-            albumArt.find('img.active').removeClass('active');
-            $('#'+currArtwork).addClass('active');
-            
-            bgArtworkUrl = $('#'+currArtwork).attr('src');
-
-            bgArtwork.css({'background-image':'url('+bgArtworkUrl+')'});
-        }
-        else
-        {
-            if( flag == 0 || flag == 1 )
-                --currIndex;
-            else
-                ++currIndex;
-        }
-    }
-
-    function initPlayer()
-	{	
-        audio = new Audio();
-
-		selectTrack(0);
-		
-		audio.loop = false;
-		
-		playPauseButton.on('click',playPause);
-		
-		sArea.mousemove(function(event){ showHover(event); });
-		
-        sArea.mouseout(hideHover);
-        
-        sArea.on('click',playFromClickedPos);
-		
-        $(audio).on('timeupdate',updateCurrTime);
-
-        playPreviousTrackButton.on('click',function(){ selectTrack(-1);} );
-        playNextTrackButton.on('click',function(){ selectTrack(1);});
-	}
-    
-	initPlayer();
+  }
 });
